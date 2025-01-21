11a55a3b8cd62c708497963481a244de2d70e45b
Bappy Mustafiz <mustafizur120133@gmail.com>
2025-01-21 18:11:51 +0600

    home page layout

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,12 @@
+import '../styles/App.css';
+import Layout from './Layout';
+import Home from './pages/Home';
 function App() {
-  return <p>fff</p>;
+  return (
+    <Layout>
+      <Home />
+    </Layout>
+  );
 }
 
 export default App;
